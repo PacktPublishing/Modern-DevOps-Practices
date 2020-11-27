1ab72cff80e9d6639e25dd3b7cb28fa108068cfb
root <root@bharatmicrosystems1c.mylabserver.com>
2020-11-27 06:42:51 +0000

    Updated chapter 2

---
+++ ch2/docker-compose/app.py
@@ -0,0 +1,23 @@
+import time
+
+import redis
+from flask import Flask
+from datetime import datetime
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_last_visited():
+    try:
+        last_visited = cache.getset('last_visited',str(datetime.now().strftime("%Y-%m-%d, %H:%M:%S")))
+        if last_visited is None:
+            return cache.getset('last_visited',str(datetime.now().strftime("%Y-%m-%d, %H:%M:%S")))
+        return last_visited
+    except redis.exceptions.ConnectionError as e:
+        raise e
+
+@app.route('/')
+def index():
+    last_visited = str(get_last_visited().decode('utf-8'))
+    return 'Hi there! This page was last visited on {}.\n'.format(last_visited)

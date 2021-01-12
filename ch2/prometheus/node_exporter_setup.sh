8efea5bed1812a86898427912b3aa465cc89f95a
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2021-01-12 10:00:54 +0000

    Updated

---
+++ ch2/prometheus/node_exporter_setup.sh
@@ -0,0 +1,8 @@
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.15.1/node_exporter-0.15.1.linux-amd64.tar.gz
+tar xvf node_exporter-0.15.1.linux-amd64.tar.gz
+cp node_exporter-0.15.1.linux-amd64/node_exporter /usr/local/bin
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-0.15.1.linux-amd64.tar.gz node_exporter-0.15.1.linux-amd64
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl start node_exporter

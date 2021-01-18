e6c858900bed6522cc06525b6098a632cedee415
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2021-01-18 08:05:28 +0000

    Updated

---
+++ ch7/lamp-stack/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>PHP to MQSQL</title>
+</head>
+<body>
+<?php
+mysql_connect('db', 'root', 'Password@1') or die('Could not connect the database : Username or password incorrect');
+echo 'Database Connected successfully';
+?>
+</body>
+</html>
--- ch7/setup-ansible-terraform/main.tf
+++ ch7/setup-ansible-terraform/main.tf
@@ -139,6 +139,19 @@ resource "azurerm_network_security_group" "web_nsg" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+
     tags = {}
 }
 
@@ -206,6 +219,18 @@ resource "azurerm_network_security_group" "db_nsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "SQL"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3306"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
